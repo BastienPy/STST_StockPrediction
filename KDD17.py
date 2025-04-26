11e425c55313c20dd44a17064e607c303c057d91
Anthony Miceli <anthony080601@gmail.com>
2025-04-26 00:41:00 -0400

    kdd17

---
+++ KDD17.py
@@ -0,0 +1,37 @@
+import yfinance as yf
+import pandas as pd
+import os
+
+# liste des 50 tickers selon l'article (KDD17 - Zhang et al., 2017)
+tickers = [
+    "BHP", "DOW", "RIO", "SYT", "VALE",       
+    "AMZN", "CMCSA", "DIS", "HD", "TM",       
+    "CVX", "PTR", "RDS-B", "TOT", "XOM",      
+    "BAC", "BRK-B", "JPM", "SPY", "WFC",     
+    "JNJ", "MRK", "NVS", "PFE", "UNH",       
+    "BA", "GE", "MA", "MMM", "UPS",           
+    "KO", "MO", "PEP", "PG", "WMT",           
+    "AAPL", "GOOGL", "INTC", "MSFT", "ORCL",  
+    "CHL", "DCM", "NTT", "T", "VZ",           
+    "D", "DUK", "EXC", "NGG", "SO"           
+]
+
+start_date = "2007-01-01"
+end_date = "2016-12-31"
+output_dir = "KDD"
+os.makedirs(output_dir, exist_ok=True)
+
+for ticker in tickers:
+    
+    print(f"Téléchargement de {ticker}...")
+    try:
+        df = yf.download(ticker, start=start_date, end=end_date, auto_adjust=False)
+        df = df[["Open", "High", "Low", "Close", "Adj Close", "Volume"]]
+        df = df.dropna()
+        df.reset_index(inplace=True)
+        filename = f"{ticker.replace('^','').replace('/','-')}.csv"
+        df.to_csv(os.path.join(output_dir, filename), index=False)
+        print(f"Sauvegardé: {filename}")
+        
+    except Exception as e:
+        print(f"Erreur pour {ticker} : {e}")
